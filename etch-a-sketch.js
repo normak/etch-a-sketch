68687ecbbef9982cafa034523a4db0392d588394
normak <km123456789@gmail.com>
2016-12-03 21:21:45 -0600

    Added colorize feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	colorize = false;
 	createGrid();
 	$(".box").hover(draw);
 });
@@ -12,11 +13,36 @@ function createGrid() {
 }
 
 function draw() {
-	$(this).css({"background-color": "black"})
+	
+	if (colorize==false) {
+		$(this).css({"background-color": "black"})
+	}
+	else {
+		$(this).css({"background-color": getRandomColor()})
+	}
 }
 
 function erase() {
 	$(".box").css({"background-color": "white"})
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setColorize() {
+	if (colorize) {
+		colorize = false;
+	}
+	else {
+		colorize = true;
+	}
+}
+
 $('#erase').on('click', erase);
+$('#colorize').on('click', setColorize);
